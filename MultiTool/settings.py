85bebd845a149d9a7294b7b772d23e87ffe285ef
konrad-paszek <konradpaszek.dev@gmail.com>
2024-11-16 14:24:39 +0100

    review fixes

--- MultiTool/settings.py
+++ MultiTool/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-
+from decouple import config
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-c4((75o+_ue!00agp5!hc^5-&($wfk7=z&7-1_th%ue**&u5k)'
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
--- expenses/models.py
+++ expenses/models.py
@@ -1,23 +1,22 @@
 from django.db import models
 
-HOME_EXPENSE_CATEGORIES = {
-    "RENT": "Rent/Mortgage",
-    "UTIL": "Utilities",
-    "FOOD": "Groceries/Food",
-    "TRANS": "Transportation",
-    "MED": "Medical/Healthcare",
-    "INS": "Insurance",
-    "ENT": "Entertainment",
-    "EDU": "Education",
-    "SAV": "Savings/Investment",
-    "OTH": "Other Expenses"
-}
-
 class Expense(models.Model):
+    CATEGORY_CHOICES = (
+        ('RENT', 'Rent/Mortgage'),
+        ('UTIL', 'Utilities'),
+        ('FOOD', 'Groceries/Food'),
+        ('TRANS', 'Transportation'),
+        ('MED', 'Medical/Healthcare'),
+        ('INS', 'Insurance'),
+        ('ENT', 'Entertainment'),
+        ('EDU', 'Education'),
+        ('SAV', 'Savings/Investment'),
+        ('OTH', 'Other Expenses'),
+    )
     title = models.CharField(max_length=30)
-    expected_cost = models.DecimalField(max_digits=5, decimal_places=2)
-    real_cost = models.DecimalField(max_digits=5, decimal_places=2)
+    expected_cost = models.DecimalField(max_digits=8, decimal_places=2)
+    real_cost = models.DecimalField(max_digits=8, decimal_places=2)
     deadline = models.DateField()
     pay_date = models.DateField()
-    category = models.CharField(max_length=10, choices=HOME_EXPENSE_CATEGORIES)
+    category = models.CharField(max_length=10, choices=CATEGORY_CHOICES)
 
