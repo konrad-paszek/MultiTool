b3beff80673734d0b0d06dbf81bb7d30c3b04487
konrad-paszek <konradpaszek.dev@gmail.com>
2024-11-18 21:24:04 +0100

    add expense table

--- expenses/urls.py
+++ expenses/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import HomeView
+from .views import HomeView, ExpensesListView
 
 urlpatterns = [
     path("", HomeView.as_view(), name="home-page"),
+    path("expenses", ExpensesListView.as_view(), name="expenses")
 ]
