959ca43e5fde4cd1ba3771771cd26fc37213f6e4
konrad-paszek <konradpaszek.dev@gmail.com>
2024-11-21 22:00:47 +0100

    Add expense table (#2)
    * add expense table
    
    * review fix

--- expenses/views.py
+++ expenses/views.py
@@ -1,5 +1,13 @@
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView
+from .models import Expense
 
 
 class HomeView(TemplateView):
     template_name = "base.html"
+
+class ExpensesListView(ListView):
+    template_name = "expenses/expenses.html"
+    context_object_name = "expenses"
+
+    def get_queryset(self):
+        return Expense.objects.all()
