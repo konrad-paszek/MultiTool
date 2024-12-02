f33a479a9c5e47bed886e4affad52dbd07b29140
konrad-paszek <konradpaszek.dev@gmail.com>
2024-12-02 18:36:21 +0100

    task#4

--- expenses/urls.py
+++ expenses/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
 
-from .views import HomeView, ExpensesListView, DraftListView
+from .views import HomeView, ExpensesListView, DraftListView, DraftListCreateView, DraftDeleteView, DraftUpdateView, DraftListDetailView
 
 urlpatterns = [
     path("", HomeView.as_view(), name="home-page"),
     path("expenses/", ExpensesListView.as_view(), name="expenses"),
     path("drafts/", DraftListView.as_view(), name="drafts"),
+    path("drafts-create/", DraftListCreateView.as_view(), name="add-draft-list"),
+    path("drafts-delete/<int:pk>/", DraftDeleteView.as_view(), name="delete-draft-list"),
+    path("drafts-update/<int:pk>/", DraftUpdateView.as_view(), name="edit-draft-list"),
+    path("drafts-detail/<int:pk>/", DraftListDetailView.as_view(), name="draft-detail")
 ]
